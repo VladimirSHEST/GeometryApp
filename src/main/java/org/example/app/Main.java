105ba4ece852699f12b198d60349af09ab466c31
VladimirSHEST <162320414+VladimirSHEST@users.noreply.github.com>
2024-10-20 09:17:17 +0400

    Update Main.java

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println("Rectangle Area: " + rectangle.getArea());
         System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
 
-        Triangle triangle = new Triangle(3, 4, 11);
+        Triangle triangle = new Triangle(3, 4, 20);
         System.out.println("Triangle Area: " + triangle.getArea());
         System.out.println("Triangle Perimeter: " + triangle.getPerimeter());
 
